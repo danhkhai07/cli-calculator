05e6e319312f26861a4ff2def4988f9b073193c2
danhkhai07 <aliveomg@gmail.com>
2025-05-16 16:35:14 +0700

    fix class error

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
+#include <string>
+
 #include "interface/interface.h"
 
 int main(){
-    CLIC::homeMenu(0);
+    CLIC::printTitle();
+    CLIC::menu("0");
+    while (true){
+        std::string input;
+        std::cout << "> ";
+        std::cin >> input;
+
+        if (CLIC::resolveOption(input) == CLIC::Options::Option4) return 0;
+        CLIC::menu(input);
+    }
     return 0;
 }
