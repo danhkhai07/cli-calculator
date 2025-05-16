a292d501480e769c827a54cdf5535c66f7963ec3
danhkhai07 <aliveomg@gmail.com>
2025-05-16 14:27:36 +0700

    create CMakeLists.txt's and interface.h & .cpp

---
+++ include/interface/interface.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "interface.h"
+
+void CLIC::homeMenu(int q){
+    system("cls");
+    
+    switch (q){
+        case 0:
+            std::cout << "1. Calculator\n";
+    }
+}
+
+void CLIC::printTitle(){
+
+}
---
+++ include/interface/interface.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace CLIC {
+    void homeMenu(int q);
+
+    void printTitle();
+}
