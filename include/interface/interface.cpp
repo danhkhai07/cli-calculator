620ea1fa29b5750cd9da5c808e0bcb8ff48120f3
danhkhai07 <aliveomg@gmail.com>
2025-05-16 16:34:16 +0700

    fix class error

--- include/interface/interface.cpp
+++ include/interface/interface.cpp
@@ -4,92 +4,65 @@
 
 #include "interface.h"
 
-/// @brief 
-enum Options {
-    Option_Invalid,
-    Option_Back,
-    Option0,
-    Option1,
-    Option1_1,
-    Option1_2,
-    Option1_3,
-    Option1_4,
-    Option2,
-    Option3,
-    Option4
-};
-
-/// @brief 
-/// @param input 
-/// @return
-Options resolveOption(const std::string& input);
-
-/// @brief 
-/// @param q 
-void CLIC::menu(std::string q);
-
-/// @brief 
-void CLIC::printTitle();
-
-Options resolveOption(const std::string& input) {
-    static const std::map<std::string, Options> optionStrings = {
+CLIC::Options CLIC::resolveOption(const std::string& input) {
+    static const std::map<std::string, CLIC::Options> optionStrings = {
     // Numbered options
-    {"1", Option1},
-        {"1.1", Option1_1},
-        {"1.2", Option1_2},
-        {"1.3", Option1_3},
+    {"1", CLIC::Options::Option1},
+        {"1.1", CLIC::Options::Option1_1},
+        {"1.2", CLIC::Options::Option1_2},
+        {"1.3", CLIC::Options::Option1_3},
 
-    {"2", Option2},
-    {"3", Option3},
+    {"2", CLIC::Options::Option2},
+    {"3", CLIC::Options::Option3},
 
-    // Home option
-    {"0", Option0},
-    {"home", Option0},
+    // Home Option
+    {"0", CLIC::Options::Option0},
+    {"home", CLIC::Options::Option0},
     
     // Exit option
-    {"4", Option4},
-    {"exit", Option4},
+    {"4", CLIC::Options::Option4},
+    {"exit", CLIC::Options::Option4},
     
     // Other options
-    {"back", Option_Back}
+    {"back", CLIC::Options::Option_Back}
     };
 
     auto itr = optionStrings.find(input);
     if (itr != optionStrings.end()) {
         return itr->second;
     }
-    return Option_Invalid;
+    return CLIC::Options::Option_Invalid;
 }
 
 void CLIC::menu(std::string q){
     system("cls");
 
     switch (resolveOption(q)){
-        case Option0:
+        case CLIC::Options::Option0:
             std::cout << "1. Calculator\n2. About us\n3. Guide\n4. Exit\n";
             break;
-        case Option1:
+        case CLIC::Options::Option1:
             std::cout << "1.1. Progressive Calculator\n1.2 String-processed calculator\n1.3 Solve for X\n";
             break;
-        case Option1_1:
+        case CLIC::Options::Option1_1:
             std::cout << "No content.\n";
             break;
-        case Option1_2:
+        case CLIC::Options::Option1_2:
             std::cout << "No content.\n";
             break;
-        case Option1_3:
+        case CLIC::Options::Option1_3:
             std::cout << "No content.\n";
             break;
-        case Option2:
+        case CLIC::Options::Option2:
             std::cout << "No content.\n";
             break;
-        case Option3:
+        case CLIC::Options::Option3:
             std::cout << "No content.\n";
             break;
-        case Option4:
+        case CLIC::Options::Option4:
             system("^C");
             break;
-        case Option_Back:
+        case CLIC::Options::Option_Back:
             std::cout << "No content.\n";
             break;
         default:
--- include/interface/interface.h
+++ include/interface/interface.h
@@ -3,6 +3,22 @@
 #include <string> 
 
 namespace CLIC {
+    enum class Options {
+        Option_Invalid,
+        Option_Back,
+        Option0,
+        Option1,
+        Option1_1,
+        Option1_2,
+        Option1_3,
+        Option1_4,
+        Option2,
+        Option3,
+        Option4
+    };
+
+    Options resolveOption(const std::string& input);
+
     void menu(std::string q);
 
     void printTitle();
