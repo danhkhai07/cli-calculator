866a00da22d011620ec9e4a11a54803e93fe3ecc
danhkhai07 <aliveomg@gmail.com>
2025-05-19 07:30:04 +0700

    add content to about'

--- src/info/info.cpp
+++ src/info/info.cpp
@@ -14,12 +14,7 @@ void CLICalc::info::helpCmd(){
 }
 
 void CLICalc::info::aboutCmd(){
-    std::cout << "Type direct string command to navigate (case insesitive):" << "\n";
-    std::cout << "\thelp                Show available commands." << "\n";
-    std::cout << "\tprogressiveCalc     Use the progressive calculator." << "\n";
-    std::cout << "\tstringCalc          Use the string-processing calculator." << "\n";
-    std::cout << "\tsolveForX           Use the solve-for-X calculator. " << "\n";
-    std::cout << "\tabout               Show credibility and other info." << "\n";
-    std::cout << "\texit                Exit the program." << "\n";
+    std::cout << "CLI CALCULATOR - Written by danhkhai07\n";
+    std::cout << "Github repository: https://github.com/danhkhai07/cli-calculator/tree/main\n";
     return;
 }
